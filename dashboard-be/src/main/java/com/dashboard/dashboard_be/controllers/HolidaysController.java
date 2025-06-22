70656b06c1402adb58a10b3075c1c930d53b46bd
Oleksii Nehoi <iplesha7@gmail.com>
2025-06-22 15:51:42 +0200

    connected to DB

--- dashboard-be/src/main/java/com/dashboard/dashboard_be/controllers/HolidaysController.java
+++ dashboard-be/src/main/java/com/dashboard/dashboard_be/controllers/HolidaysController.java
@@ -1,4 +1,18 @@
 package com.dashboard.dashboard_be.controllers;
 
+import com.dashboard.dashboard_be.services.HolidaysService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class HolidaysController {
+    private HolidaysService holidaysService;
+
+    @GetMapping("/getHolidayList")
+    public String getHolidayList() {
+
+        return holidaysService.getHolidayList();
+    }
 }
---
+++ dashboard-be/src/main/java/com/dashboard/dashboard_be/services/HolidaysService.java
@@ -0,0 +1,11 @@
+package com.dashboard.dashboard_be.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HolidaysService {
+
+    public String getHolidayList() {
+        return "Hello";
+    }
+}
